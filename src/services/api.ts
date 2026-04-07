943a26e2ebc6a6afe4bccf8e30fc53c1beb0ce2d
Suyogya Shrestha <suyogyashrestha@Suyogyas-MacBook-Air.local>
2026-04-07 10:22:36 +0545

    render in frontend2

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://nepcha-server.onrender.com/api",
 });
 // Automatically attach admin token to every request if it exists
 api.interceptors.request.use((config) => {
